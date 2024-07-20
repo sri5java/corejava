08fc9ba881eec1d927df4ea777a3409c303384c4
sri5java <66857658+sri5java@users.noreply.github.com>
2024-07-20 20:23:57 +0530

    Add files via upload

---
+++ Java8features.java
@@ -0,0 +1,12 @@
+
+20/07/2024 
+
+Java 8 Features link
+
+https://docs.google.com/document/d/1NuLHRhNB-YHrub-9an5mRoAhAyj35dqgR0RV7qmREbY/edit
+
+Java 8 Features Youtube link for 
+https://www.youtube.com/watch?v=ZDnrKzHJtDg&list=PLs_0qG9mop44_ZoU5FUnhwAqk_vM3kc7f
+
+
+
